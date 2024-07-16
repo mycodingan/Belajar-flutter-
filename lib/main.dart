a342890af671cccee7851ee48f9aaefba2fd4df1
RPL's TEAM <joshuapplg@gmail.com>
2024-07-16 11:03:55 +0700

    belajar flutter

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Halo!'),
+      ),
+      body: Center(
+        child: Text('Hai, nama saya Joshua Wahyu'),
+      ),
+    );
+  }
+}
