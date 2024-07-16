8c19cbfa902fb81904c525d32b02bd2c3affb28b
RPL's TEAM <joshuapplg@gmail.com>
2024-07-16 15:29:55 +0700

    belajar flutter Develop#1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
+// menjalankan metod run app
 
 class MyApp extends StatelessWidget {
   @override
@@ -17,9 +18,31 @@ class MyHomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Halo!'),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.blue, const Color.fromARGB(255, 176, 44, 39)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.notifications),
+            onPressed: () {},
+          ),
+        ],
       ),
       body: Center(
-        child: Text('Hai, nama saya Joshua Wahyu'),
+        child: Text(
+          'Hai, nama saya Joshua Wahyu',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.blueAccent,
+          ),
+        ),
       ),
     );
   }
